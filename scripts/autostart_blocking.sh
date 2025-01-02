5451f989e81498f98eb361e0567fdc8c9fbc5c91
zekar47 <>
2025-01-02 02:16:40 -0600

    fixed an error in autostart_blocking script

--- scripts/autostart_blocking.sh
+++ scripts/autostart_blocking.sh
@@ -5,6 +5,6 @@
 #xrandr --output Virtual-1 --mode 1920x1080
 
 # For my desktop
-# xrandr --newmode "1920x1080_240" 583.2 1920 1928 1960 2000 1080 1201 1209 1215 +Hsync -VSync
-# xrandr --addmode HDMI-1 1920x1080_240
-# xrandr --output HDMI-1 1920x1080_240
+xrandr --newmode "1920x1080_240" 583.2 1920 1928 1960 2000 1080 1201 1209 1215 +Hsync -VSync
+xrandr --addmode HDMI-1 1920x1080_240
+xrandr --output HDMI-1 --mode 1920x1080_240
